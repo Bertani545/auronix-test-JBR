2a607d33f6e70cd78f307fa276e4fa44a243017e
Bertani545 <jbrbertani@gmail.com>
2024-12-11 16:21:29 -0600

    Filter non-alive characters

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const printRickAndMortyAliveCharacters = async function()
     const charatcersObj = await fetch("https://rickandmortyapi.com/api/character")
                                     .then((response) => response.json())
                                     .then((charsJSON) => charsJSON.results);
-    console.log(charatcersObj)
+    const aliveChars = charatcersObj.filter((char) => char.status === 'Alive');
+
+
 }
 
 printRickAndMortyAliveCharacters();
