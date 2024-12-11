38baf9689abc108aa09903136117882ec63053bb
Bertani545 <jbrbertani@gmail.com>
2024-12-11 16:58:56 -0600

    Error managment and improvements

--- main.js
+++ main.js
@@ -3,27 +3,37 @@
 
 const getRickAndMortyAliveCharacters = async function()
 {
-    // ---- Specifications --------
-    const charatcersObj = await fetch("https://rickandmortyapi.com/api/character")
-                                    .then((response) => response.json())
-                                    .then((charsJSON) => charsJSON.results);  
-    const aliveCharacters = charatcersObj.filter((char) => char.status === 'Alive');
+    try{
+        // ---- Specifications --------
+        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const charatcersJSON = await response.json();
+        const charatcersObj = charatcersJSON.results;
 
-    // Replaces all sequences of white spaces with a single underscore
-    const regexAllSpaces = /\s+/gi;
-    const modifiedNames = aliveCharacters.map((char) => {
-        char.name = char.name.replace(regexAllSpaces, '_');
-        return char
-    });
-    
-    for(character of modifiedNames) console.log(character.name);
-    
+        const aliveCharacters = charatcersObj.filter((char) => char.status === 'Alive');
 
-    // ---- Return format --------
-    const returnObj = modifiedNames.map((char) => {
-        return {id: char.id, name: char.name, status: char.status, gender: char.gender};
-    })
-    return await JSON.stringify(returnObj);
+        // Replaces all sequences of white spaces with a single underscore
+        const regexAllSpaces = /\s+/g;
+        const modifiedNames = aliveCharacters.map((char) => {
+            const modChar = {...char};
+            modChar.name = modChar.name.replace(regexAllSpaces, '_');
+            return modChar;
+        });
+
+        for(let character of modifiedNames) console.log(character.name);
+        
+
+        // ---- Return format --------
+        const returnObj = modifiedNames.map((char) => {
+            return {id: char.id, name: char.name, status: char.status, gender: char.gender};
+        })
+
+        return JSON.stringify({results: returnObj}, null, 2);
+    }
+    catch(error)
+    {
+        console.error("Unable to complete call: ", error);
+        return JSON.stringify({results: "Error"});
+    }
 }
 
 getRickAndMortyAliveCharacters().then(console.log)
