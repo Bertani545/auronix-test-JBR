ad73c26aaa95f09fbf7033e0473e52332f88e170
Bertani545 <jbrbertani@gmail.com>
2024-12-11 16:13:56 -0600

    API call

--- main.js
+++ main.js
@@ -4,4 +4,7 @@
 const printRickAndMortyAliveCharacters = function()
 {
 
+    const charatcersObj = fetch("https://rickandmortyapi.com/api/character");
+    charatcersObj.then((response) => response.json())
+                .then((charsJSON) => console.log(result))
 }
